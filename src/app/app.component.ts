8900e3efc7ba42af5ce19b3a3f41b4d7374b05a2
Kurainode <kurainode@gmail.com>
2020-03-27 23:45:19 +0300

    Added code for Basic Part of task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'task 1';
+  images: NasaItem[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get("https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image")
+    .subscribe((x: NasaResponse) => this.images = x.collection.items.slice(0, 40))
+    
+  }
+}
+
+interface NasaResponse {
+  collection: NasaResponseBody
+}
+
+interface NasaResponseBody {
+  items: NasaItem[]
+}
+
+interface NasaItem {
+  data:NasaData[],
+  links:NasaLink[]
+}
+
+interface NasaData {
+  title:string,
+  description:string
+}
+
+interface NasaLink {
+  href:string
 }
